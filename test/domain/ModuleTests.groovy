3a006fb388c11fea8a452b7f1bcd36ff1b53f429
nabilh <nabil.hijazi@gmail.com>
2015-10-13 21:41:14 -0700

    Created ModuleTests with a failing test

---
+++ test/domain/ModuleTests.groovy
@@ -0,0 +1,14 @@
+package domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+class ModuleTests {
+
+	@Test
+	public void testCreateModule() {
+		Module m = new Module ("A", "B")
+	}
+
+}
